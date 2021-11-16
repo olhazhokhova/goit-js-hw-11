29c9e9023d58ee471a5640f0a810217a70e9d6dd
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-11-16 22:01:02 +0200

    add task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+
+import fetch from './js/fetch';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const { searchQuery, searchButton } = e.currentTarget.elements;
+
+  fetch(searchQuery.value).then(renderCardsMarkup);
+});
+
+function renderCardsMarkup(data) {
+  const markup = data.hits
+    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+      return `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+                <b>${likes}</b>
+            </p>
+            <p class="info-item">
+                <b>${views}</b>
+            </p>
+            <p class="info-item">
+                <b>${comments}</b>
+            </p>
+            <p class="info-item">
+                <b>${downloads}</b>
+            </p>
+        </div>
+    </div>`;
+    })
+    .join('');
+
+  gallery.innerHTML = markup;
+}
+
+// Notiflix.Notify.success('Sol lucet omnibus');
+// Notiflix.Notify.failure('Qui timide rogat docet negare');
+// Notiflix.Notify.warning('Memento te hominem esse');
+// Notiflix.Notify.info('Cogito ergo sum');
---
+++ src/js/fetch.js
@@ -0,0 +1,19 @@
+import Notiflix from 'notiflix';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '24372714-1e07211a49a3cb54d7764ff43';
+
+const fetchImages = query => {
+  return fetch(
+    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
+  ).then(response => {
+    if (!response.ok) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+    }
+    return response.json();
+  });
+};
+
+export default fetchImages;
