aa355ae84f642dcbc2c453b30fe3942d1ba92321
Olha Zhokhova <zhokhova.olha@gmail.com>
2021-11-19 18:58:32 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import fetch from './js/fetch';
 
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
+const moreBtn = document.querySelector('.load-more');
+
+let page = 1;
+let totalHits = 0;
+
+window.addEventListener('DOMContentLoaded', () => {
+  moreBtn.classList.add('hide');
+  form.elements.searchButton.disabled = true;
+  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+});
+
+form.elements.searchQuery.addEventListener('input', e => {
+  form.elements.searchButton.disabled = e.currentTarget.value.length > 0 ? false : true;
+});
 
 form.addEventListener('submit', e => {
   e.preventDefault();
+  page = 1;
+  gallery.innerHTML = '';
+  moreBtn.classList.add('hide');
+  const { searchQuery } = e.currentTarget.elements;
+  fetch(searchQuery.value, page).then(data => {
+    totalHits = data.totalHits;
+    if (page === 1) {
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+    renderCardsMarkup(data);
+    totalHits > 0 && moreBtn.classList.remove('hide');
+  });
+});
 
-  const { searchQuery, searchButton } = e.currentTarget.elements;
-
-  fetch(searchQuery.value).then(renderCardsMarkup);
+moreBtn.addEventListener('click', () => {
+  const { searchQuery } = form.elements;
+  page += 1;
+  fetch(searchQuery.value, page).then(data => {
+    renderCardsMarkup(data);
+    totalHits -= 40;
+    if (totalHits <= 40) {
+      moreBtn.classList.add('hide');
+    }
+  });
 });
 
 function renderCardsMarkup(data) {
   const markup = data.hits
-    .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
+    .map(({ largeImageURL, webformatURL, tags, likes, views, comments, downloads }) => {
       return `<div class="photo-card">
-        <div class="img"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></div>
+        <div class="img"><a href="${largeImageURL}"><img src="${webformatURL}" alt="${tags}" loading="lazy" /></a></div>
         <div class="info">
             <p class="info-item">
                 <b>Likes</b>
@@ -41,10 +77,11 @@ function renderCardsMarkup(data) {
     })
     .join('');
 
-  gallery.innerHTML = markup;
+  if (data.totalHits === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  } else {
+    gallery.insertAdjacentHTML('beforeend', markup);
+  }
 }
-
-// Notiflix.Notify.success('Sol lucet omnibus');
-// Notiflix.Notify.failure('Qui timide rogat docet negare');
-// Notiflix.Notify.warning('Memento te hominem esse');
-// Notiflix.Notify.info('Cogito ergo sum');
--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,9 +3,9 @@ import Notiflix from 'notiflix';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '24372714-1e07211a49a3cb54d7764ff43';
 
-const fetchImages = query => {
+const fetchImages = (query, page) => {
   return fetch(
-    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`,
+    `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`,
   ).then(response => {
     if (!response.ok) {
       Notiflix.Notify.failure(
